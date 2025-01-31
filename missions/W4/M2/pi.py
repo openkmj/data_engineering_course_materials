e89864d4b4b88114bfc146b1198ff62dee8be171
openkmj <openkmj@gmail.com>
2025-01-31 18:57:27 +0900

    20250131

---
+++ missions/W4/M2/pi.py
@@ -0,0 +1,33 @@
+import sys
+from random import random
+from operator import add
+
+from pyspark.sql import SparkSession
+
+# master: spark://spark-master:7077
+# sparkDriver port: 4040
+if __name__ == "__main__":
+    """
+    Usage: pi [partitions]
+    """
+
+    spark = (
+        SparkSession.builder.master("spark://localhost:7077")
+        .appName("PythonPi")
+        .config("spark.driver.host", "localhost")
+        .config("spark.driver.port", "4040")
+        .getOrCreate()
+    )
+
+    partitions = int(sys.argv[1]) if len(sys.argv) > 1 else 2
+    n = 100000 * partitions
+
+    def f(_: int) -> float:
+        x = random() * 2 - 1
+        y = random() * 2 - 1
+        return 1 if x**2 + y**2 <= 1 else 0
+
+    count = (
+        spark.sparkContext.parallelize(range(1, n + 1), partitions).map(f).reduce(add)
+    )
+    print("Pi is roughly %f" % (4.0 * count / n))
